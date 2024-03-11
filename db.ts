95cca79bc5992377415aec43c8caae6fc6cb5a7c
nisha4L <kyawzint917@gmail.com>
2024-03-11 15:29:21 +0630

    install jose . login end point

--- db.ts
+++ db.ts
@@ -1,10 +1,10 @@
-import { Client } from "pg";
+import { Client, QueryResult } from "pg";
 import { loadEnvConfig } from "@next/env";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
-export async function getClient() {
+export async function getClient() : Promise<Client> {
 
     const client = new Client({
         user: process.env.POSTGRES_USER,
@@ -15,3 +15,11 @@ export async function getClient() {
     });
     return client;
 };
+
+export async function sql(sql: string, values?: Array<any>): Promise<QueryResult<any>> {
+    const client = await getClient();
+    await client.connect();
+    const res = await client.query(sql, values);
+    await client.end();
+    return res;
+}
