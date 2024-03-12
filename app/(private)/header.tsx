3975ed914236247c09580ceca6b7b5a51b9397ab
nisha4L <kyawzint917@gmail.com>
2024-03-12 13:42:57 +0630

    swr & fetcher

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,5 +1,25 @@
-export default function Header() {
-    return (
-        <header>Header</header>
-    )
+"use client";
+
+import useSWR from "swr";
+const fetcher = async (url: RequestInfo | URL) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const msg = "An error occurred while fetching the data";
+    const info = await res.json();
+    const status = res.status;
+    const erorr = new Error(msg);
+    console.error(info, status);
+    throw erorr;
+  }
+  return res.json();
 };
+
+export default function Header() {
+  const { data, error, isLoading } = useSWR("api/users/profile", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading....</div>;
+  console.log(data);
+
+    return <header>{ data.data.username}</header>;
+}
