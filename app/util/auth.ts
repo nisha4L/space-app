a64810a35dd3f587e0f9e2cd4b5e859aeb948adc
nisha4L <kyawzint917@gmail.com>
2024-03-13 08:07:13 +0630

    fix 500 error

--- app/util/auth.ts
+++ app/util/auth.ts
@@ -5,6 +5,8 @@ export async function getJWTPayload() {
     const cookieStore = cookies()
     const token = cookieStore.get("jwt-token")
     const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+    console.log(1)
     const { payload, protectedHeader } = await jwtVerify(token?.value!, secret)
+    console.log(2, payload);
     return payload;
 };
--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,8 @@ export async function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname;
 
     const authenticatedAPIRoutes = [
-        pathname.startsWith("/api/users")
+        pathname.startsWith("/api/users"),
+        pathname.startsWith("/api/posts")
     ]
 
     if (authenticatedAPIRoutes.includes(true)) {
@@ -20,7 +21,7 @@ export async function middleware(request: NextRequest) {
             await jwtVerify(cookie.value, secret);
         } catch (error) {
             console.error(error);
-            return NextResponse.json({ error: "internal server error" }, { status: 500 });
+            return NextResponse.json({ error: "Unauthenticated" }, { status: 401 });
         }
     }
 }
